99f1ea4f2c94c336cc3a217b1176963fef81431b
vova <volodymyr.tymoshchuk@gmail.com>
2023-12-11 22:24:58 -0800

    Add field names
    - Add names to loco and station sides
    - Add one more loco

--- RRLibs/Wireless.h
+++ RRLibs/Wireless.h
@@ -8,15 +8,22 @@
 class Wireless {
   private:
     static const int RF_NAME_LENGTH = 6;
-    static const int MAX_NAME_LENGTH = 10;
-    static const int MAX_FIELD_COUNT = 8; //nRF24 don't support more than 32 bytes payload
+    static const int MAX_NAME_LENGTH = 7;
+    static const int RF24_PACKET_LEN = 32;
+    static const int MAX_FIELD_COUNT = 4;
+    static const int MAX_FLIP = 10;
+    static const int ID_NAMES = 0x00;
+    static const int ID_FIELDS = 0x80;
+
     uint8_t radioName[RF_NAME_LENGTH];
     char fieldNames[MAX_NAME_LENGTH * MAX_FIELD_COUNT];
     float fieldValues[MAX_FIELD_COUNT];
+    uint8_t packet[RF24_PACKET_LEN];
     int fieldCount;
     Timer timer;
     int sent;
     int lost;
+    int flip;
   public:
     typedef void (* Callback)(char code, float value);
     Wireless(int fieldCount, char* radioName, int cePin, int csnPin);
