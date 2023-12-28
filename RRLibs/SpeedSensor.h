d9a35d6d9caa4ce238cce9b288c7c5a5cfd484ab
vova <volodymyr.tymoshchuk@gmail.com>
2023-12-27 23:21:49 -0800

    Fix Speed Sensor
    - Tested on 948 only

--- RRLibs/SpeedSensor.h
+++ RRLibs/SpeedSensor.h
@@ -5,23 +5,29 @@
  * 
  */
 #include <SimpleKalmanFilter.h>
+#include "Timer.h"
 
 
 class SpeedSensor {
   private:
-    const long MAX_PERIOD = 3000;
+    const long MAX_PERIOD = 1500;
     SimpleKalmanFilter simpleKalmanFilter;
     int pin;
     double wheelDia;
     double clicksPerRotation;
     double speed;
+    long distance;
     double rotationPerSecond;
+    Timer timer;
   public:
     SpeedSensor(int _pin, double _clicksPerRotation, double _wheelDia) : 
-      pin(_pin), clicksPerRotation(_clicksPerRotation), wheelDia(_wheelDia), simpleKalmanFilter(5, 5, 0.5) {
+      pin(_pin), clicksPerRotation(_clicksPerRotation), wheelDia(_wheelDia), simpleKalmanFilter(1, 1, 0.1) {
     }
     float getSpeed() {
       return this->speed;
+    }
+    long getDistance() {
+      return this->distance;
     }
     void setup();
     void loop();
