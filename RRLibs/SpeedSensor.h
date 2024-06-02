f20f073606e966b792bc079d8da1ecc52395425f
vova <volodymyr.tymoshchuk@gmail.com>
2024-06-01 17:52:08 -0700

    Push peripherals to RRLibs
    - Push motor there
    - Push both implementations of SpeedSensor there
    - Push base class for servors there too
    - Rename Led to Light

--- RRLibs/SpeedSensor.h
+++ RRLibs/SpeedSensor.h
@@ -5,11 +5,17 @@
  * 
  */
 #include "Timer.h"
+#include <SimpleKalmanFilter.h>
 
 
 void speedHandler();
 
-class SpeedSensor {
+class SpeedSensorBase {
+  public:
+    virtual void update();
+};
+
+class SpeedSensor: public SpeedSensorBase {
   private:
     const int updatePeriod;
     const float distancePerClick;
@@ -48,8 +54,61 @@ class SpeedSensor {
     }
 };
 
-extern SpeedSensor speedSensor;
+class SpeedSensorLegacy: public SpeedSensorBase {
+  private:
+    const long MAX_PERIOD = 3000;
+    const int DEBOUNCE_THRESHOLD = 20;
+    const double SCALE = 2;
+    SimpleKalmanFilter filter;
+    int pin;
+
+    volatile long period;
+    volatile long lastTimestamp;
+
+    double speed;
+    double rotationPerSecond;
+
+  public:
+    double measurementError;
+    SpeedSensorLegacy(int pin) : pin(pin), filter(5, 5, 0.5) {
+    }
+    void setup() {
+      pinMode(pin, INPUT_PULLUP);
+      attachInterrupt(digitalPinToInterrupt(pin), speedHandler, FALLING);
+    }
+    void setKalmanError(double value) {
+      measurementError = value;
+      filter.setMeasurementError(value);
+    }
+    void setKalmanNoise(double value) {
+      filter.setProcessNoise(value);
+    }
+    float getSpeedRaw() {
+      return rotationPerSecond;
+    }
+    float getSpeed() {
+      return speed;
+    }
+    void loop() {
+      volatile long localPeriod = period;
+      if ( (millis() - lastTimestamp) < min(localPeriod * 2, MAX_PERIOD) )
+        rotationPerSecond = SCALE * 1000.0 / localPeriod;
+      else
+        rotationPerSecond = 0;
+      speed = filter.updateEstimate(rotationPerSecond);
+    }
+    void update() {
+      long now = millis();
+      uint16_t since = now - lastTimestamp;
+      if (since > DEBOUNCE_THRESHOLD) {
+        period = since;
+        lastTimestamp = now;
+      }
+    }
+};
+
+extern SpeedSensorBase *speedSensorPtr;
 
 void speedHandler() {
-  speedSensor.update();
+  speedSensorPtr->update();
 }
