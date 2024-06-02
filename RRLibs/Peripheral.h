2cc03a2271f4b6f15ebd665781f2d25f159406a7
vova <volodymyr.tymoshchuk@gmail.com>
2024-06-01 17:52:08 -0700

    Push peripherals to RRLibs
    - Push motor there
    - Push both implementations of SpeedSensor there
    - Push base class for servors there too
    - Rename Led to Light

--- RRLibs/Peripheral.h
+++ RRLibs/Peripheral.h
@@ -1,1 +1,64 @@
- 
+#pragma once
+#include <Servo.h>
+
+
+class Light {
+    uint8_t pin;
+  public:
+    Light(int pin): pin(pin) {};
+    void setup() {
+      pinMode(pin, OUTPUT);
+    }
+    void apply(bool on) {
+      if (on)
+        digitalWrite(pin, HIGH);
+      else
+        digitalWrite(pin, LOW);
+    }
+};
+
+class ServoBase {
+  protected:
+    int pin;
+    Servo servo;
+  public:
+    ServoBase(int pin): pin(pin) {}
+
+    void setup() {
+      pinMode(pin, OUTPUT);
+      servo.attach(pin);
+    }
+
+    virtual void apply(uint16_t value) {
+      servo.write(value);
+    }
+};
+
+class Motor {
+    const float MIN_THR = 10;
+    const int pin_back;
+    const int pin_fowd;
+  public:
+    Motor(int pin_back, int pin_fowd): pin_back(pin_back), pin_fowd(pin_fowd) {}
+
+    void setup() {
+      pinMode(pin_back, OUTPUT);
+      pinMode(pin_fowd, OUTPUT);
+    }
+
+    void apply(int direction, int throttle) {
+      throttle = map(throttle, 0, 100, MIN_THR, 255);
+      if (throttle == MIN_THR)
+        throttle = 0;
+      if (direction == 0) {
+        digitalWrite(pin_back, LOW);
+        digitalWrite(pin_fowd, LOW);
+      } else if (direction == 1) {
+        digitalWrite(pin_back, LOW);
+        analogWrite(pin_fowd, throttle);
+      } else {
+        analogWrite(pin_back, throttle);
+        digitalWrite(pin_fowd, LOW);
+      }
+    }
+};
