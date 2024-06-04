9331a3cd7b5027e75edd7b05155b766e9c9c6787
vova <volodymyr.tymoshchuk@gmail.com>
2024-06-04 00:42:46 -0700

    Change Comms to RCCLoco
    - Incapsulate network comms into RCCLoco
    - Expose couple functions for each loco to override
    - Update all locos
    - Push Loco State to a separate file

--- RRLibs/Peripheral.h
+++ RRLibs/Peripheral.h
@@ -62,3 +62,52 @@ class Motor {
       }
     }
 };
+
+class TemperatureSensor {
+  private:
+    const int THERMISTOR_B = 3977;
+    const int THERMISTOR_T0 = 25;
+    const int THERMISTOR_RT0 = 10000;
+    const int EXTERNAL_RESISTOR = 10000;
+    const float KELVIN_BASE = 273.15;
+    int pin;
+
+  public:
+    TemperatureSensor(const int pin): pin(pin) {}
+
+    void setup() {
+      pinMode(pin, INPUT);
+    }
+
+    float read() {
+      float voltage = (float)1023 / analogRead(pin) - 1;
+      float resistance = EXTERNAL_RESISTOR / voltage;
+      float temperature = resistance / THERMISTOR_RT0;
+      temperature = log(temperature);
+      temperature /= THERMISTOR_B;
+      temperature += 1.0 / (THERMISTOR_T0 + KELVIN_BASE);
+      temperature = 1.0 / temperature;
+      temperature -= KELVIN_BASE;
+      return temperature;
+    }
+};
+
+class PreassureSensor {
+  private:
+    int pin;
+
+  public:
+    PreassureSensor(int pin): pin(pin) {}
+    void setup() {
+      pinMode(pin, INPUT);
+    }
+    float read() {
+      const float bitPerVolt = 1024 / 5;
+      const float psiMax = 100;
+      const float voltsMin = 0.48;
+      const float voltsRange = 4.5 - voltsMin;
+      const float bitsMin = voltsMin * bitPerVolt;
+      float raw = analogRead(pin);
+      return (raw - bitsMin) / (voltsRange * bitPerVolt / psiMax);
+    }
+};
