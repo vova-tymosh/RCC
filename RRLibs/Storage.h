f5c7f16eb7c6b2299268af546dceeef80060698e
vova <volodymyr.tymoshchuk@gmail.com>
2024-06-06 19:27:26 -0700

    Refactor Storage
    - Make it single file/class for EEPROM and Flash
    - Push common code to RRlib

--- RRLibs/Storage.h
+++ RRLibs/Storage.h
@@ -1,26 +1,90 @@
+#pragma once
+#ifdef ARDUINO_ARCH_NRF52
+#include <Adafruit_LittleFS.h>
+#include <InternalFileSystem.h>
+#else
 #include <EEPROM.h>
+#endif
+
+
+#ifdef ARDUINO_ARCH_NRF52
 
 class Storage {
-  const int ADDR_VALIDATION = 0;
-  const int ADDR_DATA = 1;
+    const int SIZE = 256;
+    const char *FILENAME = "/rcc.cfg";
+    Adafruit_LittleFS_Namespace::File file;
+    const int FILE_O_READ = Adafruit_LittleFS_Namespace::FILE_O_READ;
+    const int FILE_O_WRITE = Adafruit_LittleFS_Namespace::FILE_O_WRITE;
 
   public:
-    uint16_t restore() {
-      uint16_t data = 0;
-      uint8_t validation = 0;
-      EEPROM.get(ADDR_VALIDATION, validation);
-      if (validation == 0xFF) {
-        validation = 0;
-        EEPROM.put(ADDR_VALIDATION, validation);
-        EEPROM.put(ADDR_DATA, data);
-        Serial.println(F("First time EEPROM init"));
-      } else {
-        EEPROM.get(ADDR_DATA, data);
+    Storage(): file(InternalFS) {}
+
+    void setup() {
+      InternalFS.begin();
+      file.open(FILENAME, FILE_O_READ);
+      if (!file) {
+        uint32_t data = 0;
+        file.open(FILENAME, FILE_O_WRITE);
+        for (int i = 0; i < SIZE / sizeof(data); i++)
+          file.write((uint8_t*)&data, sizeof(data));
+        file.close();
+      }
+    }
+
+    uint16_t restore(uint16_t offset = 0) {
+      file.open(FILENAME, FILE_O_READ);
+      file.seek(offset);
+      uint16_t data;
+      if (file) {
+        file.read(&data, sizeof(data));
+        file.close();
       }
       return data;
     }
 
-    void save(uint16_t data) {
-      EEPROM.put(ADDR_DATA, data);
+    void save(uint16_t data, uint16_t offset = 0) {
+      file.open(FILENAME, FILE_O_WRITE);
+      file.seek(offset);
+      file.write((uint8_t*)&data, sizeof(data));
+      file.close();
     }
 };
+
+#else
+
+class Storage {
+  const int SIZE = 256;
+  const int ADDR_VALIDATION = 0;
+  const int ADDR_DATA = 1;
+
+  public:
+    void setup() {
+      uint8_t validation = 0;
+      EEPROM.get(ADDR_VALIDATION, validation);
+      if (validation == 0xFF) {
+        for (int i = 0; i < SIZE; i++) {
+          EEPROM.write(i, 0);
+        }
+        Serial.println(F("First time EEPROM init"));
+      }
+    }
+
+    uint16_t restore(uint16_t offset = 0) {
+      if (offset < SIZE) {
+        uint16_t data = 0;
+        setup();
+        EEPROM.get(ADDR_DATA + offset, data);
+        return data;
+      }
+    }
+
+    void save(uint16_t data, uint16_t offset = 0) {
+      if (offset < SIZE) {
+        EEPROM.put(ADDR_DATA + offset, data);
+      }
+    }
+};
+
+#endif
+
+extern Storage storage;
