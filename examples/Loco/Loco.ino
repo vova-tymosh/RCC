e178635a93de6d96867f729ea8e7a5809586442e
vova_tymosh <volodymyr.tymoshchuk@gmail.com>
2024-12-21 17:27:20 -0800

    Fix prev commit

--- examples/Loco/Loco.ino
+++ examples/Loco/Loco.ino
@@ -3,12 +3,13 @@
  *
  *
  */
-#include "RCCLoco.h"
+#include <WiFi.h>
 #include "Timer.h"
 #include "Secrets.h"
 
 #define RCC_NO_STATION
-#include "Transport.h"
+#include "RCCLoco.h"
+
 
 
 
@@ -16,13 +17,14 @@ void connectToWifi()
 {
     WiFi.begin(SECRET_SSID, SECRET_PWD);
     int start = millis();
+    Serial.print("Connecting to wifi.");
     while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
+        delay(100);
         Serial.print(".");
     }
     int connectionTime = millis() - start;
-    Serial.print("\nConnected in: ");
-    Serial.print(connectionTime/1000);
+    Serial.print(" Done in ");
+    Serial.print((float)connectionTime/1000);
     Serial.println("s");
 }
 
@@ -33,41 +35,31 @@ class TestLoco : public RCCLoco
 public:
     using RCCLoco::RCCLoco;
 
-    void onFunction(char code, bool value)
+    void onFunction(bool activate, uint8_t code)
     {
-        Serial.println("Function: " + String((int)code) + "/" + String(value));
+        log(String("Function: ") + code + ((activate) ? " ON" : " OFF"));
     }
 
     void onThrottle(uint8_t direction, uint8_t throttle)
     {
+        log(String("Throttle: ") + throttle + " Dir: " + direction);
         state.speed = throttle;
     }
 };
 TestLoco loco;
-Transport transport(&loco);
 
-
-
-void setupSerial()
-{
-    Serial.begin(115200);
-    delay(50);
-    // for (int i = 0; i < 100 || !Serial; i++);
-    Serial.println(F("Started"));
-}
 
 void setup()
 {
-    setupSerial();
+    Serial.begin(115200);
+    delay(50);
     connectToWifi();
-    transport.begin();
-    loco.setup();
 
-    // loco.debug = true;
+    loco.debugLevel = 1;
+    loco.setup();
 }
 
 void loop()
 {
     loco.loop();
-    transport.loop();
 }
---
+++ examples/Loco/Secrets.example.h
@@ -0,0 +1,10 @@
+/*
+ * Put your wifi SSID and password instead of YOUR-SSID and YOUR-PASSWORD. 
+ * Keep the quotes as they are.
+ * Rename the file to Secrets.h
+ * Start Arduino IDE, open .ino file in this folder, click upload.
+ * Enjoy!
+ */
+
+#define SECRET_SSID "YOUR-SSID"
+#define SECRET_PWD "YOUR-PASSWORD"
