f42c154cc4ceaf3e7d9c78990ba46fbe5bcc373b
vova <volodymyr.tymoshchuk@gmail.com>
2025-06-07 13:28:52 -0700

    Add licence header to all

--- test/TestLoco/TestLoco.ino
+++ test/TestLoco/TestLoco.ino
@@ -1,8 +1,12 @@
 /*
- * RCC Example Locomotive
+ * Copyright (c) 2024-2025 Volodymyr "Vova" Tymoshchuk
+ * Distributed under MIT licence, https://github.com/vova-tymosh/RCC/blob/main/LICENSE
+ * For more details go to https://github.com/vova-tymosh/RCC
  *
- *
+ * The above copyright notice shall be included in all
+ * copies or substantial portions of the Software.
  */
+
 // #define RCC_DEBUG 2
 #include "Motherboard.h"
 #include "Peripheral.h"
--- test/TestLocoMin/TestLocoMin.ino
+++ test/TestLocoMin/TestLocoMin.ino
@@ -1,7 +1,10 @@
 /*
- * RCC Example Locomotive
+ * Copyright (c) 2024-2025 Volodymyr "Vova" Tymoshchuk
+ * Distributed under MIT licence, https://github.com/vova-tymosh/RCC/blob/main/LICENSE
+ * For more details go to https://github.com/vova-tymosh/RCC
  *
- *
+ * The above copyright notice shall be included in all
+ * copies or substantial portions of the Software.
  */
 #define NRF_CE 19
 #define NRF_CSN 18
